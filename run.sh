acadc6500323d19017e7cd91085edbf76d5115dd
Carlos Fuch <httpcarlosx@gmail.com>
2025-02-28 10:54:36 -0300

    update: melhorias no spript bash

--- run.sh
+++ run.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+usage() {
+    echo "Uso: $0 --dias <N_DIAS>"
+    exit 1
+}
+
+if [ "$1" != "--dias" ] || [ -z "$2" ]; then
+    usage
+fi
+
+DAYS="$2"
+
 source venv/bin/activate
-python3 app.py
 pip3 install -r requirements.txt
+python3 app.py $DAYS
